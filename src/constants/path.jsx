b931b74bd09f990654480d269f4340cf102f97cc
saugjong0521 <149376211+saugjong0521@users.noreply.github.com>
2025-03-27 20:28:57 +0900

    (20250327) update_whitepaper

--- src/constants/path.jsx
+++ src/constants/path.jsx
@@ -2,7 +2,7 @@ const PATH = {
 
     COMM_TELE: "https://t.me/HASHCOWOfficial",
     COMM_X: "https://x.com/HCOW_Official",
-    WHITEPAPER: "https://drive.google.com/file/d/1C2L4qFGGDZzKzUAbkd7rZaJAm9Q65PPN/view?usp=drive_link",
+    WHITEPAPER: "https://www.hash-cow.io/whitepaper.pdf",
     HASHGAMES: "https://www.hashgames.net/",
 
 };
