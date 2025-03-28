648a134787a0a8acfb2e6ebbe4f0e54d4b8b8d7c
saugjong0521 <149376211+saugjong0521@users.noreply.github.com>
2025-03-28 15:14:54 +0900

    (20250328) revert_tailwind-config_object-position

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
                 'header-bg': "url('/src/assets/header_bg.png')",
                 'about-bg': "url('/src/assets/about_bg.png')",
             },
+            objectPosition: {
+                'hashgames' : '-400px center'
+            },
         },
     },
 };
